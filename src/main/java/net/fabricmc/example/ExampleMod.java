7f2880bc4cb19c7528b28e9ac648cdd097915cac
matthyno <34276633+MatthyPlayz@users.noreply.github.com>
2020-12-03 17:53:58 -0500

    Add files via upload

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -1,14 +1,22 @@
 package net.fabricmc.example;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class ExampleMod implements ModInitializer {
+
+	public static final Item EXAMPLE_ITEM = new Item(new FabricItemSettings());
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		System.out.println("Hello Fabric world!");
+		System.out.println("hello world!!");
+		Registry.register(Registry.ITEM, new Identifier("tutorial", "fabric_item"), EXAMPLE_ITEM);
 	}
 }
